549ef3994ebf17fce8117a732cd5196ee6c115f9
Varun-Babu <itsme.varun.26@gmail.com>
2022-12-06 07:04:45 +0530

    add

--- src/main/java/com/example/productapp_backend/controller/Controller.java
+++ src/main/java/com/example/productapp_backend/controller/Controller.java
@@ -2,13 +2,21 @@ package com.example.productapp_backend.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
-    @GetMapping("/")
-    public String home(){
+    @PostMapping("/")
+    public String Home(){
         return "home";
     }
+
+    @PostMapping("/add")
+    public String Add(){
+        return "Add";
+    }
+
+
 }
