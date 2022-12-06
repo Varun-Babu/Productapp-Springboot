078533fdbee33013d7785eb2599943c409402b45
Varun-Babu <itsme.varun.26@gmail.com>
2022-12-06 07:01:27 +0530

    home

--- src/main/java/com/example/productapp_backend/controller/Controller.java
+++ src/main/java/com/example/productapp_backend/controller/Controller.java
@@ -1,4 +1,14 @@
 package com.example.productapp_backend.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
 }
